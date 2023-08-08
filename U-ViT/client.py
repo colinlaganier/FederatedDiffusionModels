401613a7114a4d92b2fbcd174487b7451654ac27
Colin Laganier <cul18@ic.ac.uk>
2023-08-08 11:36:29 +0100

    Added U-ViT and start of Flwr simulation w/ Ray

---
+++ U-ViT/client.py
@@ -0,0 +1,55 @@
+# Flower client, adapted from Pytorch quickstart example
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, cid: str, fed_dir_data: str):
+        self.cid = cid
+        self.fed_dir = Path(fed_dir_data)
+        self.properties: Dict[str, Scalar] = {"tensor_type": "numpy.ndarray"}
+
+        # Instantiate model
+        self.net = Net()
+
+        # Determine device
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def get_parameters(self, config):
+        return get_params(self.net)
+
+    def fit(self, parameters, config):
+        set_params(self.net, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+        trainloader = get_dataloader(
+            self.fed_dir,
+            self.cid,
+            is_train=True,
+            batch_size=config["batch_size"],
+            workers=num_workers,
+        )
+
+        # Send model to device
+        self.net.to(self.device)
+
+        # Train
+        train(self.net, trainloader, epochs=config["epochs"], device=self.device)
+
+        # Return local model and statistics
+        return get_params(self.net), len(trainloader.dataset), {}
+
+    def evaluate(self, parameters, config):
+        set_params(self.net, parameters)
+
+        # Load data for this client and get trainloader
+        num_workers = int(ray.get_runtime_context().get_assigned_resources()["CPU"])
+        valloader = get_dataloader(
+            self.fed_dir, self.cid, is_train=False, batch_size=50, workers=num_workers
+        )
+
+        # Send model to device
+        self.net.to(self.device)
+
+        # Evaluate
+        loss, accuracy = test(self.net, valloader, device=self.device)
+
+        # Return statistics
+        return float(loss), len(valloader.dataset), {"accuracy": float(accuracy)}
